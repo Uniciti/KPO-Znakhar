102a4204e2862c62365f10f402625c6a513c22fb
YarokovistY <yarokovisty@gmail.com>
2024-04-08 16:12:58 +0700

    delete my lab

--- Lab14/app/src/main/java/com/example/lab14/domain/usecase/GetCityItemUseCases.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lab14.domain.usecase
-
-import com.example.lab14.data.RepositoryCityImpl
-import com.example.lab14.domain.entity.CityItem
-
-class GetCityItemUseCases(private val repository: RepositoryCityImpl) {
-    operator fun invoke(cityId: Int) = repository.getCityItem(cityId)
-}
--- Lab14/app/src/main/java/com/example/lab14/domain/usecase/GetCityListUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lab14.domain.usecase
-
-import com.example.lab14.data.RepositoryCityImpl
-
-class GetCityListUseCase(private val repository: RepositoryCityImpl) {
-    operator fun invoke() = repository.getCityList()
-}
